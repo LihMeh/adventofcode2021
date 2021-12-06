fe0ffa2106d63d26ed9e18f25103d7e63ea18b0a
mikhail.polovnev <mikhail.polovnev@team.wrike.com>
2021-12-06 09:15:00 +0300

    Solve 06.2

--- src/main/kotlin/day06/Day06.kt
+++ src/main/kotlin/day06/Day06.kt
@@ -1,50 +1,62 @@
 package day06
 
-import java.util.*
-
-class Fish(var age: Int) {
-    init {
-        check(age > 0)
-    }
-
-    fun liveDays(days: Int) {
-        check(days <= age)
-        age -= days
-    }
-
-    fun resetDays() {
-        check(age == 0)
-        age = 6
-    }
-}
-
-fun parseInput(input: String): List<Fish> {
+fun parseInput(input: String): List<Int> {
     return input.splitToSequence(",")
         .filter { it.isNotBlank() }
-        .map { Fish(it.toInt()) }
+        .map { it.toInt() }
         .toList()
 }
 
-fun task1(input: String, daysToPass: Int): Int {
-    val fishList = LinkedList(parseInput(input))
+class State {
+    private val ageToFishCount: MutableMap<Int, Long> = HashMap()
+    fun addFishWithAge(age: Int, fishCount: Long) {
+        check(age >= 0)
+        check(fishCount >= 0)
+        ageToFishCount[age] = ageToFishCount.getOrDefault(age, 0) + fishCount
+    }
+
+    fun removeFishWithAge(age: Int, fishCount: Long) {
+        check(age >= 0)
+        val currentCount = getFishCountByAge(age)
+        check(fishCount >= currentCount)
+        ageToFishCount[age] = currentCount - fishCount
+    }
+
+    fun getFishCountByAge(age: Int): Long {
+        check(age >= 0)
+        return ageToFishCount.getOrDefault(age, 0)
+    }
+
+    fun totalFishCount(): Long {
+        return ageToFishCount.values
+            .sumOf { it }
+    }
+}
+
+fun task(input: String, daysToPass: Int): Long {
+    val fishList = parseInput(input)
     check(fishList.isNotEmpty())
+    check(fishList.all { it > 0 })
+
+    val state = State()
+    fishList.forEach { state.addFishWithAge(it, 1) }
 
     var daysLeft = daysToPass
     while (daysLeft > 0) {
-        var newFishesCount = 0
-        for (fish in fishList) {
-            if (fish.age > 0) {
-                fish.liveDays(1)
-            } else {
-                fish.resetDays()
-                newFishesCount++
-            }
+        val fishToGiveBirth = state.getFishCountByAge(0)
+        state.removeFishWithAge(0, fishToGiveBirth)
+
+        for (age in 1..8) {
+            val fishCount = state.getFishCountByAge(age)
+            state.removeFishWithAge(age, fishCount)
+            state.addFishWithAge(age - 1, fishCount)
         }
-        generateSequence { Fish(8) }
-            .take(newFishesCount)
-            .forEach { fishList.addFirst(it) }
+
+        state.addFishWithAge(6, fishToGiveBirth)
+        state.addFishWithAge(8, fishToGiveBirth)
+
         daysLeft--
     }
 
-    return fishList.size
+    return state.totalFishCount()
 }
--- src/test/kotlin/day06/Day06Test.kt
+++ src/test/kotlin/day06/Day06Test.kt
@@ -7,17 +7,24 @@ import kotlin.test.assertEquals
 class Day06Test {
 
     @Test
-    fun task1_example() {
-        assertEquals(1, task1("3", 3))
-        assertEquals(2, task1("3", 4))
-        assertEquals(2, task1("3", 4))
-        assertEquals(26, task1("3,4,3,1,2", 18))
-        assertEquals(5934, task1("3,4,3,1,2", 80))
+    fun task_examples() {
+        assertEquals(1, task("3", 3))
+        assertEquals(2, task("3", 4))
+        assertEquals(2, task("3", 4))
+        assertEquals(26, task("3,4,3,1,2", 18))
+        assertEquals(5934, task("3,4,3,1,2", 80))
+        assertEquals(26984457539, task("3,4,3,1,2", 256))
     }
 
     @Test
     fun task1_real() {
-        assertEquals(-1, task1(readResource("/day06_input.txt"), 80))
+        assertEquals(359344, task(readResource("/day06_input.txt"), 80))
     }
+
+    @Test
+    fun task2_real() {
+        assertEquals(1629570219571, task(readResource("/day06_input.txt"), 256))
+    }
+
 
 }
