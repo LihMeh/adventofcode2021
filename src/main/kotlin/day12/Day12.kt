d8aed393ad6087fc6a416bd4100654f99b5318de
mikhail.polovnev <mikhail.polovnev@team.wrike.com>
2021-12-12 10:15:46 +0300

    task 12.2 solved

--- src/main/kotlin/day12/Day12.kt
+++ src/main/kotlin/day12/Day12.kt
@@ -21,13 +21,35 @@ fun parseInput(input: String): Map<String, Set<String>> {
 }
 
 fun task1(input: String): Int {
+    return taskImpl(input) { path ->
+        path
+            .filter { isSmallCave(it) }
+            .toSet()
+    }
+}
+
+fun task2(input: String): Int {
+    return taskImpl(input) { path ->
+        val caveToVisitCount = countVisits(path)
+        val visitedSmallCaves = caveToVisitCount.keys
+            .filter { isSmallCave(it) }
+            .toSet()
+        val smallCavesVisitedSeveralTimes = visitedSmallCaves
+            .filter { cave -> caveToVisitCount[cave]!! > 1 }
+            .toSet()
+        check(smallCavesVisitedSeveralTimes.size < 2)
+        if (smallCavesVisitedSeveralTimes.isEmpty()) {
+            setOf("start")
+        } else {
+            visitedSmallCaves
+        }
+    }
+}
+
+fun taskImpl(input: String, forbiddenCavesStrategy: (List<String>) -> Set<String>): Int {
     val caves = parseInput(input)
     check(caves.containsKey("start"))
     check(caves.containsKey("end"))
-
-    val smallCaves = caves.keys
-        .filter { cave -> cave.none { chr -> chr.isUpperCase() } }
-        .toSet()
 
     val pathsFound = mutableListOf<List<String>>()
     val unfinishedPaths = ArrayDeque<List<String>>()
@@ -36,12 +58,10 @@ fun task1(input: String): Int {
     while (unfinishedPaths.isNotEmpty()) {
         val currentPath = unfinishedPaths.removeFirst()
         val currentCave = currentPath.last()
-
         val connectedCaves = caves[currentCave]!!
-        val visitedSmallCaves = currentPath
-            .filter { cave -> smallCaves.contains(cave) }
-            .toSet()
-        val cavesToCheck = connectedCaves.minus(visitedSmallCaves)
+        val forbiddenCaves = forbiddenCavesStrategy(currentPath)
+
+        val cavesToCheck = connectedCaves.minus(forbiddenCaves)
         for (caveToCheck in cavesToCheck) {
             val updatedPath = currentPath.toMutableList()
             updatedPath.add(caveToCheck)
@@ -54,4 +74,16 @@ fun task1(input: String): Int {
     }
 
     return pathsFound.size
+}
+
+fun isSmallCave(cave: String): Boolean {
+    return cave.none { chr -> chr.isUpperCase() }
+}
+
+fun countVisits(path: List<String>): Map<String, Int> {
+    val result = mutableMapOf<String, Int>()
+    path.forEach { cave ->
+        result[cave] = 1 + (result[cave] ?: 0)
+    }
+    return result
 }
--- src/test/kotlin/day12/Day12Test.kt
+++ src/test/kotlin/day12/Day12Test.kt
@@ -10,53 +10,118 @@ class Day12Test {
     fun task1_examples() {
         assertEquals(
             10,
-            task1("start-A\n" +
-                    "start-b\n" +
-                    "A-c\n" +
-                    "A-b\n" +
-                    "b-d\n" +
-                    "A-end\n" +
-                    "b-end")
+            task1(
+                "start-A\n" +
+                        "start-b\n" +
+                        "A-c\n" +
+                        "A-b\n" +
+                        "b-d\n" +
+                        "A-end\n" +
+                        "b-end"
+            )
         )
         assertEquals(
             19,
-            task1("dc-end\n" +
-                    "HN-start\n" +
-                    "start-kj\n" +
-                    "dc-start\n" +
-                    "dc-HN\n" +
-                    "LN-dc\n" +
-                    "HN-end\n" +
-                    "kj-sa\n" +
-                    "kj-HN\n" +
-                    "kj-dc")
+            task1(
+                "dc-end\n" +
+                        "HN-start\n" +
+                        "start-kj\n" +
+                        "dc-start\n" +
+                        "dc-HN\n" +
+                        "LN-dc\n" +
+                        "HN-end\n" +
+                        "kj-sa\n" +
+                        "kj-HN\n" +
+                        "kj-dc"
+            )
         )
         assertEquals(
             226,
-            task1("fs-end\n" +
-                    "he-DX\n" +
-                    "fs-he\n" +
-                    "start-DX\n" +
-                    "pj-DX\n" +
-                    "end-zg\n" +
-                    "zg-sl\n" +
-                    "zg-pj\n" +
-                    "pj-he\n" +
-                    "RW-he\n" +
-                    "fs-DX\n" +
-                    "pj-RW\n" +
-                    "zg-RW\n" +
-                    "start-pj\n" +
-                    "he-WI\n" +
-                    "zg-he\n" +
-                    "pj-fs\n" +
-                    "start-RW")
+            task1(
+                "fs-end\n" +
+                        "he-DX\n" +
+                        "fs-he\n" +
+                        "start-DX\n" +
+                        "pj-DX\n" +
+                        "end-zg\n" +
+                        "zg-sl\n" +
+                        "zg-pj\n" +
+                        "pj-he\n" +
+                        "RW-he\n" +
+                        "fs-DX\n" +
+                        "pj-RW\n" +
+                        "zg-RW\n" +
+                        "start-pj\n" +
+                        "he-WI\n" +
+                        "zg-he\n" +
+                        "pj-fs\n" +
+                        "start-RW"
+            )
         )
     }
 
     @Test
     fun task1_real() {
         assertEquals(4720, task1(readResource("/day12_input.txt")))
+    }
+
+    @Test
+    fun task2_example() {
+        assertEquals(
+            36,
+            task2(
+                "start-A\n" +
+                        "start-b\n" +
+                        "A-c\n" +
+                        "A-b\n" +
+                        "b-d\n" +
+                        "A-end\n" +
+                        "b-end"
+            )
+        )
+        assertEquals(
+            103,
+            task2(
+                "dc-end\n" +
+                        "HN-start\n" +
+                        "start-kj\n" +
+                        "dc-start\n" +
+                        "dc-HN\n" +
+                        "LN-dc\n" +
+                        "HN-end\n" +
+                        "kj-sa\n" +
+                        "kj-HN\n" +
+                        "kj-dc"
+            )
+        )
+        assertEquals(
+            3509,
+            task2(
+                "fs-end\n" +
+                        "he-DX\n" +
+                        "fs-he\n" +
+                        "start-DX\n" +
+                        "pj-DX\n" +
+                        "end-zg\n" +
+                        "zg-sl\n" +
+                        "zg-pj\n" +
+                        "pj-he\n" +
+                        "RW-he\n" +
+                        "fs-DX\n" +
+                        "pj-RW\n" +
+                        "zg-RW\n" +
+                        "start-pj\n" +
+                        "he-WI\n" +
+                        "zg-he\n" +
+                        "pj-fs\n" +
+                        "start-RW"
+            )
+        )
+    }
+
+    @Test
+    fun task2_real() {
+        assertEquals(147848, task2(readResource("/day12_input.txt")))
     }
 
 }
