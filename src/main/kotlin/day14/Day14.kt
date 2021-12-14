2d68217e560b1fc9b6b24708a7250d309cc70193
mikhail.polovnev <mikhail.polovnev@team.wrike.com>
2021-12-14 23:52:52 +0300

    Solved 14.2

--- src/main/kotlin/day14/Day14.kt
+++ src/main/kotlin/day14/Day14.kt
@@ -14,8 +14,16 @@ fun parseInput(input: String): Input {
     return Input(polymer, rules)
 }
 
-
 fun process(input: String, stepsLeft: Int, rules: Map<String, Char>): Map<Char, Long> {
+    return process(input, stepsLeft, rules, mutableMapOf())
+}
+
+fun process(
+    input: String,
+    stepsLeft: Int,
+    rules: Map<String, Char>,
+    cache: MutableMap<Pair<String, Int>, Map<Char, Long>>
+): Map<Char, Long> {
     check(input.length >= 2)
     check(stepsLeft >= 0)
 
@@ -27,31 +35,40 @@ fun process(input: String, stepsLeft: Int, rules: Map<String, Char>): Map<Char, 
         return result
     }
 
-    val leftPairResult: Map<Char, Long>
-    val rightPairResult: Map<Char, Long>
-    val middle: Char
     if (input.length == 2) {
+        val cacheKey = input to stepsLeft
+        val cachedResult = cache[cacheKey]
+        if (cachedResult != null) {
+            return cachedResult
+        }
+
         val left = input.first()
         val right = input.last()
-        middle = rules[input]!!
-        leftPairResult = process("" + left + middle, stepsLeft - 1, rules)
-        rightPairResult = process("" + middle + right, stepsLeft - 1, rules)
-    } else {
-        val splitIdx = input.length / 2
-        val left = input.substring(0, splitIdx + 1)
-        val right = input.substring(splitIdx)
-        middle = input[splitIdx]
-        leftPairResult = process(left, stepsLeft, rules)
-        rightPairResult = process(right, stepsLeft, rules)
+        val middle = rules[input]!!
+        val leftPairResult = process("" + left + middle, stepsLeft - 1, rules, cache)
+        val rightPairResult = process("" + middle + right, stepsLeft - 1, rules, cache)
+        val result = mutableMapOf<Char, Long>()
+        for (chr in leftPairResult.keys.union(rightPairResult.keys)) {
+            result[chr] = (leftPairResult[chr] ?: 0) + (rightPairResult[chr] ?: 0)
+        }
+        result[middle] = result[middle]!! - 1
+        cache[cacheKey] = result
+        return result
     }
 
+    val pairs = input.windowed(2)
     val result = mutableMapOf<Char, Long>()
-    for (chr in leftPairResult.keys.union(rightPairResult.keys)) {
-        result[chr] = (leftPairResult[chr] ?: 0) + (rightPairResult[chr] ?: 0)
+    for (pair in pairs) {
+        val pairResult = process(pair, stepsLeft, rules, cache)
+        pairResult.forEach { (chr, count) -> result[chr] = (result[chr] ?: 0) + count }
+        val firstChar = pair.first()
+        result[firstChar] = result[firstChar]!! - 1
     }
-    result[middle] = result[middle]!! - 1
+    val overallFirstChar = pairs.first().first()
+    result[overallFirstChar] = result[overallFirstChar]!! + 1
     return result
 }
+
 
 fun task1(inputString: String): Long {
     return taskImpl(inputString, 10)
