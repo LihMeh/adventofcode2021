8aabbfafe64612b05a0ebee8bea8c8038af01298
mikhail.polovnev <mikhail.polovnev@team.wrike.com>
2021-12-14 23:17:51 +0300

    Try solving 14.2

--- src/main/kotlin/day14/Day14.kt
+++ src/main/kotlin/day14/Day14.kt
@@ -1,6 +1,6 @@
 package day14
 
-data class Input(val polymer: String, val rules: Map<String, String>)
+data class Input(val polymer: String, val rules: Map<String, Char>)
 
 fun parseInput(input: String): Input {
     val (polymer, insertionRulesRaw) = input.split("\n\n")
@@ -8,53 +8,66 @@ fun parseInput(input: String): Input {
         .split("\n")
         .map { row ->
             val (left, right) = row.split(" -> ")
-            left to right
+            left to right.first()
         }
         .toMap()
     return Input(polymer, rules)
 }
 
-fun applyRules(input: String, rules: Map<String, String>): String {
-    val result = StringBuilder()
-    for (idx in 0 until input.length - 1) {
-        result.append(input[idx])
-        val ruleKey = input.substring(idx, idx + 2)
-        val insertCandidate = rules[ruleKey]
-        if (insertCandidate != null) {
-            result.append(insertCandidate)
+
+fun process(input: String, stepsLeft: Int, rules: Map<String, Char>): Map<Char, Long> {
+    check(input.length >= 2)
+    check(stepsLeft >= 0)
+
+    if (stepsLeft == 0) {
+        val result = mutableMapOf<Char, Long>()
+        for (chr in input) {
+            result[chr] = (result[chr] ?: 0) + 1
         }
+        return result
     }
-    result.append(input.last())
-    return result.toString()
+
+    val leftPairResult: Map<Char, Long>
+    val rightPairResult: Map<Char, Long>
+    val middle: Char
+    if (input.length == 2) {
+        val left = input.first()
+        val right = input.last()
+        middle = rules[input]!!
+        leftPairResult = process("" + left + middle, stepsLeft - 1, rules)
+        rightPairResult = process("" + middle + right, stepsLeft - 1, rules)
+    } else {
+        val splitIdx = input.length / 2
+        val left = input.substring(0, splitIdx + 1)
+        val right = input.substring(splitIdx)
+        middle = input[splitIdx]
+        leftPairResult = process(left, stepsLeft, rules)
+        rightPairResult = process(right, stepsLeft, rules)
+    }
+
+    val result = mutableMapOf<Char, Long>()
+    for (chr in leftPairResult.keys.union(rightPairResult.keys)) {
+        result[chr] = (leftPairResult[chr] ?: 0) + (rightPairResult[chr] ?: 0)
+    }
+    result[middle] = result[middle]!! - 1
+    return result
 }
 
-fun countChars(input: String): Map<Char, Long> {
-    val result = mutableMapOf<Char, Long>()
-    for (chr in input) {
-        result[chr] = (result[chr] ?: 0) + 1
-    }
-    return result
+fun task1(inputString: String): Long {
+    return taskImpl(inputString, 10)
+}
+
+fun task2(inputString: String): Long {
+    return taskImpl(inputString, 40)
 }
 
 fun taskImpl(inputString: String, steps: Int): Long {
     val input = parseInput(inputString)
 
-    var polymer = input.polymer
-    for (step in 0 until steps) {
-        polymer = applyRules(polymer, input.rules)
-    }
-
-    val charToCount = countChars(polymer)
-    val leastCommonElementCount = charToCount.minOf { it.value }
-    val mostCommonElementCount = charToCount.maxOf { it.value }
-
-    return mostCommonElementCount - leastCommonElementCount
-}
-
-fun task1(input: String): Long {
-    return taskImpl(input, 10)
-}
-
-fun task2(input: String): Long {
-    return taskImpl(input, 40)
+    val charToCount = process(input.polymer, steps, input.rules)
+    val biggestAmount = charToCount
+        .maxOf { it.value }
+    val smallestAmount = charToCount
+        .minOf { it.value }
+    return biggestAmount - smallestAmount
 }
--- src/test/kotlin/day14/Day14Test.kt
+++ src/test/kotlin/day14/Day14Test.kt
@@ -27,4 +27,57 @@ class Day14Test {
         assertEquals(-1, task2(readResource("/day14_input.txt")))
     }
 
+    @Test
+    fun process() {
+        val rules = parseInput(readResource("/day14_example.txt")).rules
+        assertEquals(
+            mapOf(
+                'N' to 1L,
+                'C' to 1L
+            ),
+            process("NC", 0, rules)
+        )
+        assertEquals(
+            mapOf(
+                'N' to 2L,
+                'C' to 1L,
+                'B' to 1L
+            ),
+            process("NNCB", 0, rules)
+        )
+        assertEquals(
+            mapOf(
+                'N' to 2L,
+                'C' to 1L
+            ),
+            process("NN", 1, rules)
+        )
+        assertEquals(
+            mapOf(
+                'N' to 2L,
+                'C' to 2L,
+                'B' to 1L
+            ),
+            process("NN", 2, rules)
+        )
+        assertEquals(
+            mapOf(
+                'N' to 2L,
+                'C' to 2L,
+                'B' to 2L,
+                'H' to 1L
+            ),
+            process("NNCB", 1, rules)
+        )
+        assertEquals(
+            mapOf(
+                'N' to 11L,
+                'B' to 23L,
+                'C' to 10L,
+                'H' to 5L
+            ),
+            process("NNCB", 4, rules)
+        )
+    }
+
 }
