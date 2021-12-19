ca457059a41b5ee3ce85031124ddbc3009de68a1
mikhail.polovnev <mikhail.polovnev@team.wrike.com>
2021-12-19 13:20:46 +0300

    19.1 in progress

---
+++ src/main/kotlin/day19/Day19.kt
@@ -0,0 +1,108 @@
+package day19
+
+typealias Point = List<Int>
+typealias ScanResult = Set<Point>
+
+fun parseInput(input: String): List<ScanResult> {
+    return input.split("\n\n")
+        .map { scannerData ->
+            val scannerStrings = scannerData.split("\n")
+            val pointStrings = scannerStrings.subList(1, scannerStrings.size)
+            pointStrings.map {
+                it.split(",")
+                    .map { it.toInt() }
+                    .toList()
+            }.toSet()
+        }
+}
+
+fun pointMinus(left: Point, right: Point): Point {
+    check(left.size == right.size)
+    return left.indices
+        .map { idx -> left[idx] - right[idx] }
+}
+
+fun pointPlus(left: Point, right: Point): Point {
+    check(left.size == right.size)
+    return left.indices
+        .map { idx -> left[idx] + right[idx] }
+}
+
+fun allPossibleDirections(input: ScanResult): List<ScanResult> {
+    val scannedRotations = mutableListOf(
+        input,
+        input.map { point -> listOf(point[0], point[2], point[1]) }.toSet(),
+        input.map { point -> listOf(point[2], point[1], point[0]) }.toSet(),
+        input.map { point -> listOf(point[1], point[0], point[2]) }.toSet()
+    )
+
+    val result = mutableListOf<ScanResult>()
+    for (rotation in scannedRotations) {
+        for (xMultiplier in listOf(-1, 1)) {
+            for (yMultiplier in listOf(-1, 1)) {
+                for (zMultiplier in listOf(-1, 1)) {
+                    val rotatedAndRedirected = mutableSetOf<Point>()
+
+                    rotatedAndRedirected.addAll(rotation
+                        .map { point ->
+                            listOf(
+                                point[0] * xMultiplier,
+                                point[1] * yMultiplier,
+                                point[2] * zMultiplier
+                            )
+                        })
+
+                    result.add(rotatedAndRedirected)
+                }
+            }
+        }
+    }
+
+    return result
+}
+
+fun task1(input: String): Int {
+    val scanners = parseInput(input)
+    check(scanners.isNotEmpty())
+
+    val scannersQueue = ArrayDeque<ScanResult>()
+    scannersQueue.addAll(scanners)
+
+    val knownBeacons = mutableSetOf<Point>()
+    knownBeacons.addAll(scannersQueue.removeFirst())
+
+    while (scannersQueue.isNotEmpty()) {
+        val nextScanner = scannersQueue.removeFirst()
+        val nextScannerDirections = allPossibleDirections(nextScanner)
+
+        val matchingDirections = nextScannerDirections
+            .filter { findIntersectionShift(knownBeacons, it, 12) != null }
+        check(matchingDirections.size <= 1)
+        if (matchingDirections.isNotEmpty()) {
+            val match = matchingDirections.first()
+            val intersectionShift = findIntersectionShift(knownBeacons, match, 12)!!
+            match
+                .map { pointPlus(it, intersectionShift) }
+                .forEach { knownBeacons.add(it) }
+        } else {
+            scannersQueue.addLast(nextScanner)
+        }
+        println(scannersQueue.size)
+    }
+
+    return knownBeacons.size
+}
+
+fun findIntersectionShift(left: ScanResult, right: ScanResult, enoughPoints: Int): Point? {
+    for (leftStartingPoint in left) {
+        val normalizedLeftPoints = left.map { pointMinus(it, leftStartingPoint) }.toSet()
+        for (rightStartingPoint in right) {
+            val normalizedRightPoints = right.map { pointMinus(it, rightStartingPoint) }.toSet()
+            val commonPoints = normalizedLeftPoints.intersect(normalizedRightPoints)
+            if (commonPoints.size >= enoughPoints) {
+                return pointMinus(leftStartingPoint, rightStartingPoint)
+            }
+        }
+    }
+    return null
+}
---
+++ src/test/kotlin/day19/Day19Test.kt
@@ -0,0 +1,134 @@
+package day19
+
+import common.readResource
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day19Test {
+
+    @Test
+    fun task1_example() {
+        assertEquals(79, task1(readResource("/day19_example.txt")))
+    }
+
+    @Test
+    fun task1_real() {
+        assertEquals(-1, task1(readResource("/day19_input.txt")))
+    }
+
+    /*
+представьте себе такой одномерный ввод:
+координата абсолтная: 0 1 2 3 4 5 6 7 8 9
+                маяк:   *     *     *
+     центры сканеров:     0     1
+
+сканер 0: -1  2  5
+сканер 1: -4 -1  2
+
+leftStartingPoint: -1
+normalizedLeft: 0 3 6
+rightStartingPoint: -4
+normalizedRight: 0 3 6
+
+targetDiff (result): leftStartingPoint - rightStartingPoint
+targetDiff: 3
+
+Почему?
+Требования к targetDiff такие: если к точке в координатах правого маяка прибавить targetDiff, получится точка в координатах левого маяка.
+
+маяк 1: -4 + 3 = -1
+маяк 4: -1 + 3 =  2
+маяк 7:  2 + 3 =  5
+
+ */
+    @Test
+    fun find_intersection_shift_example_small() {
+        assertEquals(
+            listOf(3),
+            findIntersectionShift(
+                setOf(listOf(-1), listOf(2), listOf(5)),
+                setOf(listOf(-4), listOf(-1), listOf(2)),
+                3
+            )
+        )
+    }
+
+    /*
+    абсолютная координата:  0 1 2 3 4 5 6 7 8 9
+                     маяк:    *   *     *     *
+          центры сканеров:      1         0
+
+     сканер 0: -4 -1  2
+     сканер 1: -1  1  4
+     targetDiff: -5
+     */
+    @Test
+    fun find_intersection_shift_example_big() {
+        assertEquals(
+            listOf(-5),
+            findIntersectionShift(
+                setOf(listOf(-4), listOf(-1), listOf(2)),
+                setOf(listOf(-1), listOf(1), listOf(4)),
+                2
+            )
+        )
+    }
+
+    @Test
+    fun directions() {
+        val scanners = parseInput(
+            "" +
+                    "--- scanner 0 ---\n" +
+                    "-1,-1,1\n" +
+                    "-2,-2,2\n" +
+                    "-3,-3,3\n" +
+                    "-2,-3,1\n" +
+                    "5,6,-4\n" +
+                    "8,0,7\n" +
+                    "\n" +
+                    "--- scanner 0 ---\n" +
+                    "1,-1,1\n" +
+                    "2,-2,2\n" +
+                    "3,-3,3\n" +
+                    "2,-1,3\n" +
+                    "-5,4,-6\n" +
+                    "-8,-7,0\n" +
+                    "\n" +
+                    "--- scanner 0 ---\n" +
+                    "-1,-1,-1\n" +
+                    "-2,-2,-2\n" +
+                    "-3,-3,-3\n" +
+                    "-1,-3,-2\n" +
+                    "4,6,5\n" +
+                    "-7,0,8\n" +
+                    "\n" +
+                    "--- scanner 0 ---\n" +
+                    "1,1,-1\n" +
+                    "2,2,-2\n" +
+                    "3,3,-3\n" +
+                    "1,3,-2\n" +
+                    "-4,-6,5\n" +
+                    "7,0,8\n" +
+                    "\n" +
+                    "--- scanner 0 ---\n" +
+                    "1,1,1\n" +
+                    "2,2,2\n" +
+                    "3,3,3\n" +
+                    "3,1,2\n" +
+                    "-6,-4,-5\n" +
+                    "0,7,-8"
+        )
+        for (scannerToRotate in scanners) {
+            val allDirections = allPossibleDirections(scannerToRotate)
+            val expetedButNotFoundDirections = scanners
+                .filter { !allDirections.contains(it) }
+                .toList()
+            assert(expetedButNotFoundDirections.isEmpty()) {
+                "" +
+                        "scanner to rotate:\n" + scannerToRotate + "\n" +
+                        "not found directions: \n" + expetedButNotFoundDirections + "\n"
+            }
+        }
+    }
+
+}
